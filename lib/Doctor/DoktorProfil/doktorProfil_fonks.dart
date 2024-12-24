870086feb39cfed2feb6978a25d0fbe33b72d5be
Ayberk ÖZ <ayberkooz@hotmail.com>
2024-12-24 23:03:10 +0300

    Ayberk -> doktor profil fonks

---
+++ lib/Doctor/DoktorProfil/doktorProfil_fonks.dart
@@ -0,0 +1,28 @@
+import 'package:yazilim_projesi/models/Doctors.dart';
+import 'package:yazilim_projesi/services/doctor_service.dart';
+
+class DoktorProfilFonks {
+
+  final DoctorService doctorService = DoctorService();
+
+  Future<Doctors?> loadData() async {
+    try {
+      final response = await doctorService.getDoctorByToken();
+      final Map<String, dynamic> data = response.data;
+      return Doctors.fromJson(data);
+    } catch (e) {
+      print("Hata oluştu: $e");
+      return null;
+    }
+  }
+
+  Future<void> updateDoctor(String id, Doctors doctorData) async {
+    try {
+      final response = await doctorService.updateDoctorById(id, doctorData);
+    } catch (e) {
+      print("Hata oluştu: $e");
+      return null;
+    }
+  }
+  
+}
