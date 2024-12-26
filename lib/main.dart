e116fef8069187888f4b17b8c5d28cd63a6561dc
Ayberk ÖZ <ayberkooz@hotmail.com>
2024-12-26 13:39:18 +0300

    ayberk -> doctorInfo mock json

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yazilim_projesi/Doctor/DoktorProfil/doktor_abonelik.dart';
+import 'package:yazilim_projesi/Doctor/DoktorProfil/doktor_profil.dart';
+import 'package:yazilim_projesi/Doctor/doktor_bilgi/doktor_bilgi.dart';
 import 'package:yazilim_projesi/Doctor/favori_doktor/favori_doktor.dart';
 import 'package:yazilim_projesi/Doctor/screens/Doctor_home_screen.dart';
 import 'package:yazilim_projesi/Hasta/ana_ekran/ana_ekran.dart';
@@ -7,6 +10,7 @@ import 'package:yazilim_projesi/Hasta/randevu_al/randevu_al.dart';
 import 'package:yazilim_projesi/Hasta/randevu_al/randevu_al_doktor_liste.dart';
 import 'package:yazilim_projesi/Hasta/yorum/doktor_yorum_ekran%C4%B1.dart';
 import 'package:yazilim_projesi/Hasta/yorum/hasta_yorum.dart';
+import 'package:yazilim_projesi/giris_ekran/giris_ekrani.dart';
 
 import 'models/Doctors.dart';
 
@@ -52,7 +56,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const GecmisRandevular(),
+      home: const DoctorBilgiEkran(doctorId: '',),
     );
   }
 }
